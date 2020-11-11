ac73a2817096a051ff4586874a0be362013a57b4
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-11-11 14:31:37 -0600

    Developed Node Implementation

--- Project2/Node.cpp
+++ Project2/Node.cpp
@@ -1,7 +1,28 @@
 #include "Node.h"
 
 Node::Node(const std::string& p)
+	: nextNode{ nullptr }
 {
-	payload = p;
+	this->payload = p;
 }
 
+void Node::setNextNode(Node* nxtNodePtr)
+{
+	this->nextNode = nxtNodePtr;
+}
+
+Node* Node::getNextNode()
+{
+	return this->nextNode;
+}
+
+
+void Node::setPayload(const std::string& p) 
+{
+	this->payload = p;
+}
+
+std::string Node::getPayload() 
+{
+	return this->payload;
+}
--- Project2/Node.h
+++ Project2/Node.h
@@ -3,13 +3,17 @@
 #include <string>
 
 // forward declaration of OrderedList
-class OrderedList;
+class OrderedList; //(OrderedList does not exist yet)
 
 class Node
 {
     friend class OrderedList;
 public:
     explicit Node(const std::string& p);
+    void setNextNode(Node* nxtNodePtr);
+    Node* getNextNode();
+    void setPayload(const std::string& p); //May not be used in project but good practice to have
+    std::string getPayload();
 private:
     std::string payload;
     Node* nextNode{ nullptr };
