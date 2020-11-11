72ea987dbbc2e32783c14b06673e9fc0b6ae16fd
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-11-11 16:59:33 -0600

    Finished Node testing

--- Project2/Node.h
+++ Project2/Node.h
@@ -3,18 +3,24 @@
 #include <string>
 
 // forward declaration of OrderedList
-//class OrderedList; //(OrderedList does not exist yet, uncomment after ordered list is added)
+class OrderedList; //(OrderedList does not exist yet, uncomment after ordered list is added)
 
 class Node
 {
-    //friend class OrderedList; //Allows the ordered list to directly interface with the class
+    friend class OrderedList; //Allows the ordered list to directly interface with the class
 public:
+
     explicit Node(const std::string& p);
+
+    //Methods are not needed but as the ordered list can directly reference the private vars
+    //Used if you want to encapsulate the node
     void setNextNode(Node* nxtNodePtr);
     Node* getNextNode();
-    void setPayload(const std::string& p); //May not be used in project but good practice to have
+    void setPayload(const std::string& p);
     std::string getPayload();
-    std::string toString(); //Creates a formatted debug string of the node ex. Node( Payload: "hi", nextNode: true)
+
+    //Creates a formatted debug string of the node ex. Node( Payload: "hi", nextNode: true)
+    std::string toString(); 
 private:
     std::string payload;
     Node* nextNode{ nullptr };
--- Project2/node_test_driver.cpp
+++ Project2/node_test_driver.cpp
@@ -3,4 +3,24 @@
 
 #include <iostream>
 
-int main() {}
+using namespace std;
+
+int main() {
+
+	Node a{ "a node" };
+	Node b{ "b node" };
+	Node c{ "c node" };
+
+	cout << a.toString() << endl;
+	a.setNextNode(&b);
+	cout << a.toString() << endl;
+	
+	cout << b.toString() << "Checking if b is referencible by a" << endl;
+
+	cout << a.getNextNode()->toString() << "Both this and the last string should match"<< endl;
+
+	b.setNextNode(&c);
+	cout << b.toString() << "Checking if b is referencible by a with a next node" << endl;
+
+	cout << a.getNextNode()->toString() << "Both this and the last string should match" << endl;
+}
